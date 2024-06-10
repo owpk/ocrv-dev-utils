ed8c000ee0658ea9229dd0ec8f68b43b21859bb9
Vyacheslav Vorobev <voyzvz@gmail.com>
2024-06-10 14:17:57 +0800

    fix dump script

--- postgres/dev_db_dump.sh
+++ postgres/dev_db_dump.sh
@@ -1,9 +1,19 @@
+# ип базы 172.22.10.23
+
+# логин 
+# pn_service
+ 
+# пароль
+# pn-secret
+ 
+# база 
+# pndb
+
 DB_NAME=$1
+DB_USER=pn_service
+DUMP="./dump/dump_$DB_NAME.sql"
 
-CONTAINER_NAME=$DB_NAME
-DB_USER=postgres
+pg_dump --verbose --host=172.22.10.23 --port=5432 --username=$DB_USER --format=p --encoding=UTF-8 --inserts --no-privileges --no-owner --file $DUMP -n public $DB_NAME
 
-DUMP="/tmp/$DB_NAME.pg_dev.sql"
-
-ssh dev01@dev07 "pg_dump -v -h localhost -U $DB_USER -Fc $DB_NAME" > $DUMP
-docker exec -i $CONTAINER_NAME pg_restore -U $DB_USER -v -d $DB_NAME < $DUMP
+#ssh pn_service@172.22.10.23 "pg_dump -v -h localhost -U $DB_USER -Fc $DB_NAME" > $DUMP
+# docker exec -i $CONTAINER_NAME pg_restore -U $DB_USER -v -d $DB_NAME < $DUMP
