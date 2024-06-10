2fa53f15d13c6fdbaf46a492218d1e7fdd176f7e
owpk <53643556+owpk@users.noreply.github.com>
2024-06-10 13:47:29 +0800

    updates

--- postgres/recreate-pn-db.sh
+++ postgres/recreate-pn-db.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-DB_NAME=test_db
-DB_PASS=postgres
-DB_USER=postgres
+DB_NAME=$1
+DB_PASS=$2
+DB_USER=$3
+DB_PORT=$4
 
-DUMP="$1"
-echo "Dump file: $DUMP"
+DUMP="$5"
+
+echo "connection info:"
+echo "DB_NAME:      $DB_NAME"
+echo "DB_PASS:      $DB_PASS" 
+echo "DB_USER:      $DB_USER" 
+echo "DB_PORT:      $DB_PORT" 
+echo "DUMP_FILE:    $DUMP"
 
 CONTAINER_NAME=$DB_NAME
 IMAGE=postgres:14
 
 docker container rm -f $CONTAINER_NAME
 
-docker run --name $CONTAINER_NAME -p 5432:5432 -d \
+docker run --name $CONTAINER_NAME -p 5450:5432 -d \
     -e POSTGRES_PASSWORD=$DB_PASS \
     -e POSTGRES_USER=$DB_USER \
     -e POSTGRES_DB=$DB_NAME \
