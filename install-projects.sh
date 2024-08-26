d2de96293a8a88a98108a90a1455dc871b2b65e4
user <user@astra>
2024-08-26 20:13:22 +0800

    MASTER: projects install script

---
+++ install-projects.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+projects=(
+   "deploy" 
+   "auth" 
+   "bff" 
+   "pn-backend" 
+   "pn-front" 
+   "fs-front" 
+   "fs-backend"
+)
+
+CUR=$(pwd)
+
+install() {
+   HOST="$1"
+   TARGET_DIR="$2"
+   
+   mkdir -p $TARGET_DIR
+   cd $TARGET_DIR
+   
+   echo "project list: $PR"
+
+   for i in ${projects[@]}; do
+      target="$HOST/$i.git"
+      echo "installing project $target"
+      git clone $target 
+   done
+   cd $CUR
+}
+
+# czt
+CZT=$HOME/ocrv/czt
+CZT_GIT_HOST="git@gitlab.ocrv.ru:czt-portal"
+CZT_HTTP_HOST="https://gitlab.ocrv.ru/czt-portal"
+
+CZT_HOST=$CZT_GIT_HOST
+
+install $CZT_HOST $CZT 
+
+# ext
+
+EXT=$HOME/ocrv/ext
+EXT_GIT_HOST="git@github.com:alexandr170288"
+EXT_HTTP_HOST="https://github.com/alexandr170288"
+
+EXT_HOST=$EXT_GIT_HOST
+
+projects=(
+   "auth"
+   "pn-backend"
+)
+
+install $EXT_HOST $EXT
