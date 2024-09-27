e78c0772107817d64b2ec2113f057ed2f1c3e06a
vyacheslav.vorobev <voyzvz@gmail.com>
2024-09-27 17:56:18 +0800

    MASTER: Update Nginx configuration for multiple domains, add proxy rules for portsainer and db services, remove unused assets and config files.

--- deploy/glance/run.sh
+++ deploy/glance/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+docker stop glance
+docker rm glance
 
-docker run --name glance -d --restart always -p 8080:8080 \
+docker run --name glance -d --restart always -p 8002:8080 \
   -v ./glance.yml:/app/glance.yml \
   -v /etc/timezone:/etc/timezone:ro \
   -v /etc/localtime:/etc/localtime:ro \
