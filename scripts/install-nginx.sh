300cd15f60b97402a19d70b3fa9a19719dc9285a
user <user@irk-31435-116.ocrv.com.rzd>
2024-11-26 11:56:49 +0800

    USER: Here is the commit message for the given changes:
    - Update Nginx installation script and configuration
    
    The script now enables Nginx at boot time and adds an entry to `/etc/hosts` for local development. It also updates the symlinks for `czt.conf` and `czt_ssl.conf`, as well as moves and creates a new symlink for the main `nginx.conf`.

--- scripts/install-nginx.sh
+++ scripts/install-nginx.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 sudo dnf install nginx
+sudo systemctl enable nginx
+sudo bash -c 'echo "127.0.0.1 resource.local" >> /etc/hosts'
 
 cd ../deploy/nginx/conf
-ln -s $(pwd)/conf.d/czt.conf /etc/nginx/conf.d/
-ln -s $(pwd)/conf.d/czt_ssl.conf /etc/nginx/conf.d/
+sudo ln -s $(pwd)/conf.d/czt.conf /etc/nginx/conf.d/
+sudo ln -s $(pwd)/conf.d/czt_ssl.conf /etc/nginx/conf.d/
+
 sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-ln -s $(pwd)/nginx.conf /etc/nginx/
+sudo ln -s $(pwd)/nginx.conf /etc/nginx/
+
